5f3533f6986dbdba8a4340e0a716f84fd452ca9b
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-10 16:52:48 +0300

    випадкоіе одне число за допомогою обєкта

--- less5.java
+++ less5.java
@@ -1,2 +1,11 @@
+import java.util.Random;
+
 public class less5 {
+
+    public static void main(String args []){
+
+        Random in = new Random();
+        int x = in.nextInt(6)  + 1;
+        System.out.println(x);
+    }
 }
