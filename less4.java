6544aadd62eebc7a3dabdd5eb85488208f9f2e89
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-10 16:43:39 +0300

    випадкове чмсло random

---
+++ less4.java
@@ -0,0 +1,13 @@
+public class less4 {
+
+    public static int getRandomDiceNumber() {
+        return (int) (Math.random()*6);
+    }
+    public static void main(String args[]){
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(getRandomDiceNumber());
+        }
+
+    }
+}
