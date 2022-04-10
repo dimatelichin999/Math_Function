33592e289331f3274f761e32a8424b16d4e1a8c7
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-10 16:39:27 +0300

    Випадкові число
    за допомогою алгоритма

---
+++ less3.java
@@ -0,0 +1,21 @@
+public class less3 {
+
+    public static int a = 41;
+    public static int c = 11119;
+    public static int m = 11113;
+    public static int seed = 1;
+
+    public static int getNextRandom(){
+        seed = (a * seed + c ) % m;
+        return seed;
+    }
+
+    public static void main(String args[]){
+
+        for (int i = 0 ; i <1000; i++){
+
+            System.out.println(getNextRandom());
+        }
+    }
+
+}
