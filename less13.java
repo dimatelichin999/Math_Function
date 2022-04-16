745aae6f5ca4e065bd74afe2d6b20b1fdfa28817
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-16 19:43:58 +0300

    формула площі кола

--- less13.java
+++ less13.java
@@ -2,6 +2,15 @@ public class less13 {
 
     public static void main(String args[]){
 
-        
+        int r = 2;
+        int a  = 3;
+        getArea(r,a);
+
     }
+
+    public static void getArea(int r , int a ){
+        double s = (int) (Math.PI * (Math.pow(r,2)-Math.pow(a,2)));
+        System.out.println(s);
+    }
+
 }
